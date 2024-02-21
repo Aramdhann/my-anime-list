d1bd0367a227bb7bf4af72586c17c4ed7be1d4d0
Aramdhann <radityamochamad@gmail.com>
2024-02-21 16:53:16 +0700

    update responsive card and navbar button

--- src/components/atoms/Button/NavbarButton.js
+++ src/components/atoms/Button/NavbarButton.js
@@ -1,6 +1,6 @@
 const NavbarButton = ({ buttonTitle }) => {
   return (
-    <button className="font-bold opacity-100 duration-300 ease-out hover:opacity-75 hover:ease-in">
+    <button className="font-bold opacity-100 duration-300 ease-out hover:opacity-75 hover:ease-in text-left w-fit text-xs sm:text-base">
       {buttonTitle}
     </button>
   );
--- src/components/molecules/Card/index.js
+++ src/components/molecules/Card/index.js
@@ -1,16 +1,16 @@
 import { FaStar } from "react-icons/fa6";
 import Image from "next/image";
 
-const Card = ({title, images, score, alt}) => {
+const Card = ({ title, images, score, alt }) => {
   return (
-    <div className="flex flex-col gap-2 p-2 border border-zinc-600 rounded-lg duration-100 ease-out hover:border-blue-500 hover:ease-in hover:scale-105">
+    <div className="flex w-fit max-w-[200px] flex-col gap-2 p-2 border border-zinc-600 rounded-lg duration-100 ease-out hover:border-blue-500 hover:ease-in hover:scale-105">
       <div className="relative">
         <div className="absolute bottom-0 h-[20%] bg-gradient-to-t from-black to-transparent opacity-45 rounded-b-lg w-full"></div>
         <div className="absolute p-2 flex justify-between w-full gap-1 items-center bottom-0">
           <p className="text-xs text-white">On-going</p>
-          <div className="flex gap-1 items-center px-2 py-1 bg-orange-400 rounded-lg">
-            <FaStar color="yellow" size={16} />
-            <p className="text-sm leading-none">{score}</p>
+          <div className="flex gap-1 items-center px-1 py-1 bg-orange-400 rounded-md">
+            <FaStar color="yellow" size={12} />
+            <p className="text-xs leading-none text-white">{score}</p>
           </div>
         </div>
         <Image
@@ -18,7 +18,7 @@ const Card = ({title, images, score, alt}) => {
           alt={alt}
           width={200}
           height={250}
-          className="rounded-lg"
+          className="rounded-lg max-h-[250px] object-cover"
         />
       </div>
       <p className="font-bold">{title}</p>
