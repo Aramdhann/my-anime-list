d14063de4d60829f343f3f7e958f7e081c5c1282
Aramdhann <radityamochamad@gmail.com>
2024-02-22 15:29:15 +0700

    remove animation

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,7 +1,7 @@
 const Input = ({ placeholder, onFocus, onBlur }) => {
   return (
     <input
-      className="flex rounded-lg text-zinc-800 px-3 py-1 w-[150px] sm:w-fit justify-center focus:w-full focus:ease-in ease-out duration-300"
+      className="flex rounded-lg text-zinc-800 px-3 py-1 w-[150px] sm:w-fit justify-center focus:w-full"
       placeholder={placeholder}
       onFocus={onFocus}
       onBlur={onBlur}
