84824012b42fe82e9b29a0d980ae7f1c28eed77b
Aramdhann <radityamochamad@gmail.com>
2024-02-22 14:22:51 +0700

    add input search bar in navbar

---
+++ src/components/atoms/Input/index.js
@@ -0,0 +1,9 @@
+const Input = () => {
+  return (
+    <>
+      <input className="flex rounded-lg text-zinc-800 px-2 py-1 mt-4 sm:mt-0 w-full sm:w-fit justify-center"></input>
+    </>
+  );
+};
+
+export default Input;
--- src/components/organism/Navbar/index.js
+++ src/components/organism/Navbar/index.js
@@ -1,18 +1,20 @@
 import NavbarButton from "@/components/atoms/Button/NavbarButton";
+import Input from "@/components/atoms/Input";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center w-full mb-3 bg-blue-500 p-4 sticky top-0 z-10">
-      <div className="container flex justify-between">
+    <header className="flex justify-center w-full mb-3 bg-blue-500 p-4 sticky top-0 z-10">
+      <div className="container flex flex-wrap justify-between">
         <a href="/" className="uppercase font-bold text-white text-xl">
           myanimelist
         </a>
-        <div className="flex gap-5">
+        <Input />
+        <div className="flex gap-5 mt-4 md:mt-0 w-full md:w-fit justify-center">
           <NavbarButton buttonTitle={"📃 Anime List"} />
           <NavbarButton buttonTitle={"⌛ Schedule Release"} />
         </div>
       </div>
-    </div>
+    </header>
   );
 };
 
