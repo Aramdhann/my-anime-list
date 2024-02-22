713cfb505e868b2a803759063d6185a5318a3b02
Aramdhann <radityamochamad@gmail.com>
2024-02-22 15:27:25 +0700

    fix minor input

--- src/components/molecules/Card/Header.js
+++ src/components/molecules/Card/Header.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 const Header = ({ titleList }) => {
-  return <p className="mb-3 font-bold text-blue-300">✨{titleList}</p>;
+  return (
+    <div className="flex justify-between items-center mb-3">
+      <p className="font-bold text-blue-300">✨{titleList}</p>
+      <Link
+        href="/popular"
+        className="duration-300 ease-out hover:opacity-75 hover:ease-in"
+      >
+        see all
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
