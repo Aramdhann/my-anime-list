5fdea0e74e162d37565bd3ceb2b9d2acda66be67
Aramdhann <radityamochamad@gmail.com>
2024-03-01 16:23:22 +0700

    add loading

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center">
+      <div class="custom-loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
